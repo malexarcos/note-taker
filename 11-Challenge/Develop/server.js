f39d45a3d7b4e81ce6e10da3a2ddd00572b0b03f
malexarcos <110743192+malexarcos@users.noreply.github.com>
2022-11-12 13:51:02 -0600

    added more code and added server.js

---
+++ 11-Challenge/Develop/server.js
@@ -0,0 +1,34 @@
+const express = require ('express');
+const path = require ('path');
+const app = express ();
+const db = require ('./db/db.json');
+const fs = require ('fs');
+
+app.use(express.static('./public'))
+app.use(express.urlencoded({extended: false}));
+app.use(express.json())
+
+app.get("/notes", (req,res) => {
+    res.sendFile (path.join(__dirname, "./public/notes.html"));
+})
+
+app.get("/api/notes", (req,res) => {
+    res.sendFile (path.join(__dirname, "./db/db.json"));
+})
+
+app.post("/api/notes", (req,res) => {
+    var newNote = req.body
+    console.log (newNote);
+    db.push (newNote);
+    fs.writeFile ('./db/db.json', JSON.stringify(db), () => {
+        res.json (newNote);
+    })
+});
+
+app.get ('*',(req,res)=>{
+    res.sendFile (path.join(__dirname, "./public/index.html"));
+}) 
+
+app.listen (3001, () => {
+console.log ('server is running');
+})
