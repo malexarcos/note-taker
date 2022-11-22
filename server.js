9eadc3af18e279fb2bdfee91058c2fdf591d27f3
malexarcos <110743192+malexarcos@users.noreply.github.com>
2022-11-21 22:00:19 -0600

    port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require ('path');
 const app = express ();
 const db = require ('./db/db.json');
 const fs = require ('fs');
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('./public'))
 app.use(express.urlencoded({extended: false}));
@@ -29,6 +30,6 @@ app.get ('*',(req,res)=>{
     res.sendFile (path.join(__dirname, "./public/index.html"));
 }) 
 
-app.listen (3001, () => {
+app.listen (PORT, () => {
 console.log ('server is running');
 })
